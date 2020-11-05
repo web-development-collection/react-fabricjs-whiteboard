83e61916d0b2ec1f7106005cb65a91966b99201b
ingoandelhofs <ingoandelhofs@gmail.com>
2020-11-05 19:51:01 +0100

    :tada: Setup React

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Whiteboard from "./components/Whiteboard";
+
+
+const App = () => {
+
+  return <Whiteboard />;
+}
+
+export default App;
---
+++ src/components/Whiteboard.tsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+
+interface Props {}
+
+
+class Whiteboard extends PureComponent<Props> {
+  public render() {
+    return <div>Whiteboard</div>;
+  }
+}
+
+export default Whiteboard;
